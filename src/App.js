d1befdb986bede89c45e8864d545e64c8848f138
Roy Nyaga <nyagaandreroy@gmail.com>
2020-04-29 01:22:59 +0100

    instll material ui

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from 'react'
 import './App.css'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
-function App () {
-  return (
-    <div className='App'>
-      <h1>Our app</h1>
-    </div>
-  )
+// pages
+import home from './pages/home'
+import login from './pages/login'
+import signup from './pages/signup'
+
+class App extends React.Component {
+  render () {
+    return (
+      <div className='App'>
+        <Router>
+          <Switch>
+            <Route exact path='/' component={home} />
+            <Route exact path='/login' component={login} />
+            <Route exact path='/signup' component={signup} />
+          </Switch>
+        </Router>
+      </div>
+  	)
+  }
 }
 
 export default App
