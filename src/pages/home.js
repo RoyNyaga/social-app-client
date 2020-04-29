2ecf83c3469a042dcbad6010c159a641477f314e
Roy Nyaga <nyagaandreroy@gmail.com>
2020-04-29 22:25:44 +0100

    add grid to home page to display screams

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,17 @@
 import React from 'react'
+import Grid from '@material-ui/core/Grid'
 
 export default class home extends React.Component {
   render () {
     return (
-      <div>
-        <h1>Home page</h1>
-      </div>
+      <Grid container spacing={16}>
+        <Grid item sm={8} xs={12}>
+          <p> Content </p>
+        </Grid>
+        <Grid item sm={4} xs={12}>
+          <p> Profile </p>
+        </Grid>
+      </Grid>
     )
   }
 }
