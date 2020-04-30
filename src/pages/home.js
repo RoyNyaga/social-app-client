1644bfe5673ae68684f69dc543ed2050283a93b3
Roy Nyaga <nyagaandreroy@gmail.com>
2020-04-30 01:54:37 +0100

    add axios, set a proxy on package.json file

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,12 +1,33 @@
 import React from 'react'
 import Grid from '@material-ui/core/Grid'
+import axios from 'axios'
 
 export default class home extends React.Component {
+
+  state = {
+    screams: null
+  }
+
+  componentDidMount(){
+    axios.get("/screams")
+    .then(res => {
+      console.log(res.data)
+      this.setState({
+        screams: res.data
+      })
+    })
+    .catch(err => console.log(err))
+  }
+
   render () {
+
+    var recentScreamsMarkup = this.state.screams ? (
+      this.state.screams.map(scream => <p>{scream.body}</p>)
+      ) : <p>Loading...</p>
     return (
-      <Grid container spacing={16}>
+      <Grid container spacing={10}>
         <Grid item sm={8} xs={12}>
-          <p> Content </p>
+          {recentScreamsMarkup}
         </Grid>
         <Grid item sm={4} xs={12}>
           <p> Profile </p>
